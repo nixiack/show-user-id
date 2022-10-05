c6f32088e1a25fa58fbf65598a5d47513803fba7
Mike White <nixiack@Mikes-MacBook-Pro.local>
2022-10-05 13:16:59 -0500

    added plugin files show user id

---
+++ hello.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * @package		test-plugin-Mike
+ * @author		Mike W
+ * @copyright	2022 Mike W
+ * @license		GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: test-plugin-Mike
+ * Plugin URL: http://testwoo.local/
+ * Description: Test plugin that I am creating first time
+ * Version: 1.0.0
+ * Author: Mike W
+ * Author URL: http://testwoo.local/
+ * Text Domain: Hello greeting
+ * License: GPL v2 or later
+ * License URL: http://www.gnu.org/licenses/gp1-2.0.txt
+ */
+/*
+ * This file is our first run at adding "Hello John!" to a page.
+ * 
+*/
+echo 'Hello John!';
+?>
